02aabfae9534bdd24cd7056f4fefb857c4e0db19
matt-varnam <matthew.varnam@manchester.ac.uk>
2020-06-29 15:33:41 +0100

    Update and rename ld_curve_generator_update.py to ld_curve_generator.py
    Gave a more appropriate name to the generator python scripts, and applied update to make it work correctly with new iFit master pull

--- ld_curve_generator.py
+++ ld_curve_generator.py
@@ -23,12 +23,11 @@ import numpy as np
 
 # iFit reads and analyses UV spectra of SO2
 from ifit.load_spectra import read_spectrum, average_spectra
+from ifit.parameters import Parameters
 from ifit.spectral_analysis import Analyser
 
 # iFit_mod is alterations to iFit to allow analysis of light diluted spectra
-from ifit_mod.settings import import_settings 
 from ifit_mod.synthetic_suite import Analyser_ld
-
 
 import matplotlib.pyplot as plt
 
@@ -36,26 +35,81 @@ import matplotlib.pyplot as plt
 # User settings
 # =============================================================================
 
+mode = 0
+
 # Define the directory containing all work files
-mst_dir = ('C:/Work/PhD/spectra/20180115-Stationary/Ben_format_shifted/')
+prm_dir = ('F:/Black_Hole/Data/201801_Masaya/Ryu_spectra/'+
+          '20180115_DOAS_for_camera_all/Ben_format_shifted/')
+
+#prm_dir = ('F:/Black_Hole/Data/201801_Masaya/Ryu_spectra/'+
+#           '20180114/Ben_format_shifted/')
+
+#prm_dir = ('F:/Black_Hole/Data/2012_0318_EtnaLavaFountain/Model_Input/'+
+#           'EMIL/Zenith_7.45/')
 
 # Set spectrometer name and directory
 spec_name = 'FLMS02101_2'
-spec_dir  = mst_dir + ('spectra/spectrum_')
-drk_dir   = mst_dir + ('dark/spectrum_')
+spec_dir  = prm_dir + ('spectra/spectrum_')
+drk_dir   = prm_dir + ('dark/spectrum_')
 
 # Create list of spectra numbers
 spec_num = [511,521]
+#spec_num = [210,220] 
 drk_num  = [  0,  0]
 
 # Set model parameters
-so2_grid_ppmm = np.arange(0,3100,100)
+#so2_grid_ppmm = np.arange(0,5010,20)
+#ldf_grid = np.arange(0,1.0,0.002)
+
+so2_grid_ppmm = np.arange(0,3020,100)
 ldf_grid = np.arange(0,1.0,0.1)
 
 # Set wavebands to view
 pad  = 1
 wav0 = [306,316]
 wav1 = [312,322]
+
+# =============================================================================
+# Model Setup
+# =============================================================================
+
+# Create parameter dictionary
+params = Parameters()
+
+# Add the gases
+params.add('SO2',  value=1.0e16, vary=True, xpath='Ref/SO2_295K.txt')
+params.add('O3',   value=1.0e19, vary=True, xpath='Ref/O3_233K.txt')
+params.add('Ring', value=0.1,    vary=True, xpath='Ref/Ring.txt')
+    
+# Add background polynomial parameters
+params.add('bg_poly0', value=0.0, vary=True)
+params.add('bg_poly1', value=0.0, vary=True)
+params.add('bg_poly2', value=0.0, vary=True)
+params.add('bg_poly3', value=1.0, vary=True)
+
+# Add intensity offset parameters
+#params.add('offset0', value=0.0, vary=True)
+
+# Add wavelength shift parameters
+params.add('shift0', value=0.002, vary=True)
+params.add('shift1', value=-1.1, vary=True)
+
+# Add ILS parameters
+params.add('fwem', value = 0.68 , vary = False)
+params.add('k',    value = 2.58 , vary = False)
+params.add('a_w',  value = 0.064, vary = False)
+params.add('a_k',  value = 0.71 , vary = False)
+
+# Generate the first analyser for setup
+analyser_prm = Analyser(params,
+                        fit_window = [wav0[0]-pad, wav1[1]+pad],
+                        frs_path   = 'Ref/sao2010.txt',
+                        flat_flag  = True,
+                        flat_path  = 'Spectrometer/flat_FLMS02101_2.txt',
+                        stray_flag = True,
+                        dark_flag  = True,
+                        ils_type   = 'Manual',
+                        ils_path   = 'Spectrometer/ils_params_FLMS02101.txt')
 
 # =============================================================================
 # Pre-processing
@@ -68,9 +122,6 @@ spec_fnames = [spec_dir + "{:05d}".format(num) + '.txt'for num in spec_range]
 # List of names of all dark files
 drk_range  = np.arange(drk_num[0],drk_num[1]+1)
 drk_fnames = [drk_dir + "{:05d}".format(num) + '.txt'for num in drk_range]
-
-# Create wavelength range of large fit containing two sub wavebands
-wav_mst  = [wav0[0]-pad, wav1[1]+pad]
 
 if len(spec_fnames) == 1:
     # Load single specrum
@@ -88,52 +139,38 @@ else:
     # Load spectra and average them to produce a single spectrum
     x, dark = average_spectra(drk_fnames)
 
-# Load model parameters
-common, settings = import_settings(spec_name,wav_mst)
-
-# Find the fit and stray windows
-common['fit_idx']   = np.where(np.logical_and(x > settings['w_lo'],
-                                              x < settings['w_hi']))
-common['stray_idx'] = np.where(np.logical_and(x > settings['s_lo'],
-                                              x < settings['s_hi']))
-
-# Store wide measurement grid (trimmed spectrometer grid) for later use
-meas_grid = x[common['fit_idx']]
-
-# If no stray light pixels available, turn off the flag
-if len(common['stray_idx'][0]) == 0:
-    common['stray_flag'] = False     
-else:
-    common['stray_flag'] = True
-
-#Copy dark spectrum to common
-common['dark'] = dark
-
-# Create ifit analyser
-analyser_mst = Analyser(common)
+analyser_prm.dark_spec = dark
 
 # =============================================================================
 # Run broad wavelength analysis
 # =============================================================================
 
-fit = analyser_mst.fit_spectrum([x,y], calc_od=['SO2', 'Ring', 'O3'])
-
-print(fit.params.pretty_print())
+fit_prm = analyser_prm.fit_spectrum([x,y], calc_od=['SO2', 'Ring', 'O3'])
+print(fit_prm.params.pretty_print())
 
 # =============================================================================
 # Create suite of synthetic spectra
 # =============================================================================
 
-# Create second ifit analyser 
-analyser_ld = Analyser_ld(common)
-analyser_ld.params.update_values(fit.params.popt_list())
+# Create second diluted ifit analyser for setup
+analyser_ld = Analyser_ld(params,
+                          fit_window = [wav0[0]-pad, wav1[1]+pad],
+                          frs_path   = 'Ref/sao2010.txt',
+                          flat_flag  = True,
+                          flat_path  = 'Spectrometer/flat_FLMS02101_2.txt',
+                          stray_flag = True,
+                          dark_flag  = True,
+                          ils_type   = 'Manual')
+
+analyser_ld.params.update_values(fit_prm.params.popt_list())
 analyser_ld.params.add('LDF', value = 0.0, vary = True)
+analyser_ld.interp_method = 'cubic'
 
 # Convert SO2 in ppmm to molecules/cm2
-so2_grid = np.multiply(so2_grid_ppmm , 2.463e+15)
+so2_grid = np.multiply(so2_grid_ppmm , 2.652e+15)
 
 # Use shape of grid and so2 value to produce a single array
-shape = (len(so2_grid),len(common['fit_idx'][0]),len(ldf_grid))
+shape = (len(so2_grid),len(fit_prm.spec),len(ldf_grid))
 spectra_suite = np.zeros(shape)
 
 # Create synthetic spectra by updating parameters
@@ -148,7 +185,7 @@ for i, so2 in enumerate(so2_grid):
         fit_params = analyser_ld.params.fittedvalueslist()
         
         # Create synthetic spectrum
-        spectra_suite[i,...,j] = analyser_ld.fwd_model(meas_grid,*fit_params)
+        spectra_suite[i,...,j] = analyser_ld.fwd_model(fit_prm.grid,*fit_params)
 
 # =============================================================================
 # Analyse spectra in first waveband
@@ -156,20 +193,14 @@ for i, so2 in enumerate(so2_grid):
         
 print('Analyse synthetic spectra in waveband 0')
 
-# Load model parameters
-common, settings = import_settings(spec_name,wav0)
-
-# Remove dark and stray from process as not included in synthetic spectra        
-common['dark_flag'] = False
-common['stray_flag']= False
-common['flat_flag'] = False
-
-# Define maximum and minimum index within synthetic measurement grid
-common['fit_idx']   = np.where(np.logical_and(meas_grid > settings['w_lo'],
-                                              meas_grid < settings['w_hi']))
-
-# Create new analyser for 
-analyser0 = Analyser(common)
+# Create first ifit analyser for results 
+analyser0 = Analyser(params,
+                     fit_window = wav0,
+                     frs_path   = 'Ref/sao2010.txt',
+                     flat_flag  = False,
+                     stray_flag = False,
+                     dark_flag  = False,
+                     ils_type   = 'Manual')
 
 # Create arrays to store answers
 ifit_so2_0 = np.zeros((shape[0],shape[2]))
@@ -180,7 +211,7 @@ for i, so2 in enumerate(so2_grid):
     for j, ldf in enumerate(ldf_grid):
 
         #Extract syntheteic spectrum for suite of spectra
-        spectrum = [meas_grid,spectra_suite[i,...,j]]
+        spectrum = [fit_prm.grid,spectra_suite[i,...,j]]
         
         # Analyse spectrum
         fit = analyser0.fit_spectrum(spectrum, calc_od=['SO2', 'Ring', 'O3'])
@@ -192,7 +223,8 @@ for i, so2 in enumerate(so2_grid):
     print(i)
 
 #Create new ifit_so2 with units in ppm.m
-ifit_so2_ppmm0 = np.divide(ifit_so2_0,2.463e15)
+ifit_so2_ppmm0 = np.divide(ifit_so2_0,2.652e15)
+ifit_err_ppmm0 = np.divide(ifit_err_0,2.652e15)
 
 # =============================================================================
 # Analyse spectra in second waveband
@@ -200,20 +232,15 @@ ifit_so2_ppmm0 = np.divide(ifit_so2_0,2.463e15)
 
 print('Analyse synthetic spectra in waveband 1')
 
-# Load model parameters
-common, settings = import_settings(spec_name,wav1)
 
-# Remove dark and stray from process as not included in synthetic spectra        
-common['dark_flag'] = False
-common['stray_flag']= False
-common['flat_flag'] = False
-
-# Define maximum and minimum index within synthetic measurement grid
-common['fit_idx']   = np.where(np.logical_and(meas_grid > settings['w_lo'],
-                                              meas_grid < settings['w_hi']))
-
-# Create new analyser for 
-analyser1 = Analyser(common)
+# Create second ifit analyser for results 
+analyser1 = Analyser(params,
+                     fit_window = wav1,
+                     frs_path   = 'Ref/sao2010.txt',
+                     flat_flag  = False,
+                     stray_flag = False,
+                     dark_flag  = False,
+                     ils_type   = 'Manual')
 
 # Create arrays to store answers
 ifit_so2_1 = np.zeros((shape[0],shape[2]))
@@ -224,7 +251,7 @@ for i, so2 in enumerate(so2_grid):
     for j, ldf in enumerate(ldf_grid):
         
         #Extract syntheteic spectrum for suite of spectra
-        spectrum = [meas_grid,spectra_suite[i,...,j]]
+        spectrum = [fit_prm.grid,spectra_suite[i,...,j]]
         
         fit = analyser1.fit_spectrum(spectrum, calc_od=['SO2', 'Ring', 'O3'])
         
@@ -235,7 +262,8 @@ for i, so2 in enumerate(so2_grid):
     print(i)
  
 #Create new ifit_so2 with units in ppm.m
-ifit_so2_ppmm1 = np.divide(ifit_so2_1,2.463e15)
+ifit_so2_ppmm1 = np.divide(ifit_so2_1,2.652e15)
+ifit_err_ppmm1 = np.divide(ifit_err_1,2.652e15)
 
 # =============================================================================
 # Create plot to show light dilution curves
